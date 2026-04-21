7440878ac0e4abafc23ca34ace48e42902758f65
KiliLoje <salaun.mael09@gmail.com>
2026-04-21 15:30:19 +0200

    Improve parsing and include base logic parsing

--- grammar.js
+++ grammar.js
@@ -20,9 +20,9 @@ export default grammar({
     ),
 
     _bloc: $ => choice(
-      $.format,
-      $.lookup,
-      $.display,
+      $.format_definition,
+      $.lookup_definition,
+      $.display_definition,
       $._line,
     ),
 
@@ -32,21 +32,21 @@ export default grammar({
     ),
 
 
-    format: $ => seq(
-      $.format_definition,
+    format_definition: $ => seq(
+      $.format_definition_header,
       optional($.format_type_definition),
     ),
 
-    format_definition: $ => seq(
+    format_definition_header: $ => seq(
       'Format', ':',
-      $.identifier,
+      field('name', $.identifier),
       optional($.comment),
       '\n'
     ),
 
     format_type_definition: $ => seq(
       'FormatType', '=',
-      $.format_type,
+      field('type', $.format_type),
       optional($.comment),
       '\n'
     ),
@@ -74,25 +74,25 @@ export default grammar({
       'FLOAT6'
     ),
 
-    lookup: $ => seq(
-      $.lookup_definition,
+    lookup_definition: $ => seq(
+      $.lookup_definition_header,
       repeat($.lookup_statement),
     ),
 
-    lookup_definition: $ => seq(
+    lookup_definition_header: $ => seq(
       'Lookup',':',
-      $.identifier,
+      field('name', $.identifier),
       optional($.comment),
       '\n'
     ),
 
     lookup_statement: $ => prec(1, seq(
-      $.key,
+      field('key', $.key),
       '=',
-      choice(
+      field('value', choice(
         $.value,
         prec(1, blank())
-      ),
+      )),
       optional($.comment),
       '\n'
     )),
@@ -119,13 +119,13 @@ export default grammar({
 
     value: $ => token(prec(-1, /([^/\n]|\/[^/])+/)),
 
-    display: $ => prec(1, seq(
-      $.display_header,
+    display_definition: $ => prec(1, seq(
+      $.display_definition_header,
       repeat($.conditional_display),
       optional($.default_display),
     )),
 
-    display_header: $ => seq('Display', ':', optional($.comment), '\n'),
+    display_definition_header: $ => seq('Display', ':', optional($.comment), '\n'),
 
     conditional_display: $ => seq(
       $.condition,
@@ -150,13 +150,115 @@ export default grammar({
 
     macro: $ => seq(
       '@',
-      $.identifier,
+      field('name', $.identifier),
       '(',
       $.logic,
       ')'
     ),
 
-    logic: $ => /[^?)]*/,
+    //logic: $ => /[^?)]*/,
+    logic: $ => seq(
+      $.logic_group,
+      repeat(seq(
+        'S',
+        $.logic_group
+      ))
+    ),
+
+    logic_group: $ => seq(
+      $.logic_condition,
+      repeat(seq(
+        '_',
+        $.logic_condition
+      ))
+    ),
+
+    logic_condition: $ => seq(
+      optional(seq(
+        $.logic_flag,
+        ":"
+      )),
+      choice(
+        $.logic_operand,
+        seq(
+          field('left', $.logic_operand),
+          $.logic_operator,
+          field('right', $.logic_operand)
+      ))
+    ),
+
+    logic_flag: $ => choice(
+      'P',    // Pause If
+      'R',    // Reset If
+      'Z',    // Reset Next If
+      'A',    // Add Source
+      'B',    // Sub Source
+      'C',    // Add Hits
+      'D',    // Sub Hits
+      'I',    // Add Address
+      'N',    // And Next
+      'O',    // Or Next
+      'M',    // Measured
+      'Q',    // Measured If
+      'T',    // Trigger
+      'K',    // Remember
+    ),
+
+    logic_operand: $ => choice(
+      field('recall', $.logic_recall),
+      field('mem', $.logic_mem),
+      field('value', $.logic_value)
+    ),
+
+    logic_recall: $ => /{recall}/,
+
+    logic_mem: $ => seq(
+      optional(field('type', $.logic_mem_type)),
+      "0x",
+      optional(field('size', $.logic_mem_size)), // no size == 16-bit
+      field('address', $.logic_mem_address)
+    ),
+
+    logic_mem_type: $ => choice(
+      'd',    // Delta
+      'p',    // Prior
+      'b',    // BCD
+      '~',    // Invert
+    ),
+
+    logic_mem_size: $ => choice(
+      'M',    // Bit0
+      'N',    // Bit1
+      'O',    // Bit2
+      'P',    // Bit3
+      'Q',    // Bit4
+      'R',    // Bit5
+      'S',    // Bit6
+      'T',    // Bit7
+      'L',    // Lower4
+      'U',    // Upper4
+      'H',    // 8-bit
+      ' ',    // 16-bit
+      'W',    // 24-bit
+      'X',    // 32-bit
+      'I',    // 16-bit BE
+      'J',    // 24-bit BE
+      'G',    // 32-bit BE
+      'K',    // BitCount
+    ),
+
+    logic_mem_address: $ => /[0-9a-fA-F]+/,
+
+    logic_value: $ => choice(
+      /h[0-9a-fA-F]+/,
+      /[0-9]+/
+    ),
+
+    logic_operator: $ => choice(
+      "=", "!=", "<=", "<", ">=", ">",
+      "+", "-", "*", "/", "%", "&", "^"
+    ),
+
     text: $ => token(prec(1, /([^@\n?/]|\/[^/])+/)),
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
